6d27967db03354564ae2e8a85d260df80eb686c9
Samuel-03 <68006880+Samuel-03@users.noreply.github.com>
2022-08-20 09:25:57 +0300

    Add files via upload

---
+++ arrays.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    $friends = array("kelvin", "karen", "oscar", "jim");
+    $friends[1]="Dwight";
+    echo $friends[1];
+    echo count($friends);
+     ?>
+  </body>
+</html>
